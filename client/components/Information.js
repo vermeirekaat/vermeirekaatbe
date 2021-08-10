b89cfaed34286ce307060676682d950e56eb9eba
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-08-10 17:04:27 +0200

    information responsive #3

--- client/components/Information.js
+++ client/components/Information.js
@@ -3,8 +3,11 @@ import styles from "./Information.module.css";
 export default function Information({ background }) {
     return(
         <section className={styles.container} style={{ backgroundColor: background }}>
-            <div className={styles.image}></div>
-            <p className={styles.info}>I create digital products that fullfil the user&apos;s needs. Always asking for feedback to learn and improve my skills.</p>
+            <div className={styles.background_content}>
+                <div className={styles.image}></div>
+                <p className={styles.info}>I create digital products that fullfil the user&apos;s needs. Always asking for feedback to learn and improve my skills.</p>
+            </div>
+           
 
         </section>
     )
