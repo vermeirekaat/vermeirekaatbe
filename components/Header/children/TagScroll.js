6f8ad3dc895928246da52995779a144258d15c88
vermeirekaat <kaat.vermeire@telenet.be>
2023-04-07 10:35:51 +0200

    Fix import issue

---
+++ components/Header/children/TagScroll.js
@@ -0,0 +1,44 @@
+import {
+    motion,
+    useViewportScroll,
+    useTransform,
+  } from "framer-motion";
+  import { useRef, useState, useEffect } from "react";
+  
+  import styles from "../Header.module.scss";
+  
+  export default function TagScroll({ input }) {
+    const { scrollYProgress } = useViewportScroll();
+    const ref = useRef(null);
+  
+    const [displayAmount, setDisplayAmount] = useState(1);
+  
+    const posX = useTransform(scrollYProgress, [0, 1], [0, 600]);
+  
+    useEffect(() => {
+      calculateWidth();
+    }, [displayAmount]);
+  
+    const calculateWidth = () => {
+      if (!ref.current) {
+        return;
+      }
+  
+      const width = ref.current.offsetWidth;
+  
+      const amount = Math.ceil(window.innerHeight / width);
+      setDisplayAmount(amount);
+    };
+    return (
+      <div className={styles.tagline}>
+        <motion.div style={{ x: posX }}>
+          {Array.from(Array(displayAmount), (e, i) => (
+            <span key={i} ref={ref}>
+              {input}
+            </span>
+          ))}
+        </motion.div>
+      </div>
+    );
+  }
+  
--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
     CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
     CONTENTFUL_TOKEN: process.env.CONTENTFUL_TOKEN,
     CONTENTFUL_ENVI: process.env.CONTENTFUL_ENVI,
+    IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
+  },
+
+  images: {
+    domains: [process.env.IMAGES_DOMAIN],
   },
 
   fonts: withFonts()
