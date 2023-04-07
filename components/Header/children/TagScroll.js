1bfb2269e65127303e0e146a235525cb9404bbdf
vermeirekaat <kaat.vermeire@telenet.be>
2023-04-07 11:07:28 +0200

    Add windowResize hook for calculating the amount #38

--- components/Header/children/TagScroll.js
+++ components/Header/children/TagScroll.js
@@ -1,44 +1,64 @@
-import {
-    motion,
-    useViewportScroll,
-    useTransform,
-  } from "framer-motion";
-  import { useRef, useState, useEffect } from "react";
-  
-  import styles from "../Header.module.scss";
-  
-  export default function TagScroll({ input }) {
-    const { scrollYProgress } = useViewportScroll();
-    const ref = useRef(null);
-  
-    const [displayAmount, setDisplayAmount] = useState(1);
-  
-    const posX = useTransform(scrollYProgress, [0, 1], [0, 600]);
-  
-    useEffect(() => {
-      calculateWidth();
-    }, [displayAmount]);
-  
-    const calculateWidth = () => {
-      if (!ref.current) {
-        return;
-      }
-  
-      const width = ref.current.offsetWidth;
-  
-      const amount = Math.ceil(window.innerHeight / width);
-      setDisplayAmount(amount);
-    };
-    return (
-      <div className={styles.tagline}>
-        <motion.div style={{ x: posX }}>
-          {Array.from(Array(displayAmount), (e, i) => (
-            <span key={i} ref={ref}>
-              {input}
-            </span>
-          ))}
-        </motion.div>
-      </div>
-    );
-  }
-  
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+
+import styles from "../Header.module.scss";
+
+export default function TagScroll({ input }) {
+  const { scrollYProgress } = useViewportScroll();
+  const ref = useRef(null);
+  const size = useWindowSize();
+
+  const [displayAmount, setDisplayAmount] = useState(1);
+
+  const posX = useTransform(scrollYProgress, [0, 1], [-1200, 600]);
+
+  useEffect(() => {
+    calculateWidth();
+  });
+
+  const calculateWidth = () => {
+    if (!ref.current || size.width === undefined) {
+      return;
+    }
+
+    const width = ref.current.offsetWidth;
+
+    const amount = Math.ceil(size.width / width) * 2;
+    setDisplayAmount(amount);
+  };
+
+  return (
+    <div className={styles.tagscroll}>
+      <motion.div className={styles.tagscroll__container} style={{ x: posX }}>
+        {Array.from(Array(displayAmount), (e, i) => (
+          <span key={i} ref={ref}>
+            {input}
+          </span>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
